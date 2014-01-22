d34ab6c3c9ed2522ad828d600fdf4b2e3ca77e60
srini4digit <srini.desh@gmail.com>
2014-01-21 20:27:51 -0500

    Added code to filter by Year.

--- js/script.js
+++ js/script.js
@@ -1,4 +1,13 @@
 $(".lblRdo").click(function(e){
 	
-	updateStackedBarChart($(e.currentTarget).find("input[name=rdoGender]").attr("value"));
+	updateStackedBarChart($(e.currentTarget).find("input[name=rdoGender]").attr("value"),"2012");
+});
+
+$("a.yearSelection").click(function(e){
+	e.preventDefault();
+	/* $(this).toggleClass("active"); */
+	$("a.yearSelection.active").removeClass("active");
+	$(this).addClass("active");
+	
+	updateStackedBarChart($("input[name=rdoGender]").attr("value"),$(this).attr("data-value"));
 });
