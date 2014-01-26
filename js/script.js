559d5e3bab4951f2be34ae6773112844a66a13de
srini4digit <srini.desh@gmail.com>
2014-01-25 19:07:14 -0500

    Unified the way chart updates by streamlining the filters.

--- js/script.js
+++ js/script.js
@@ -1,13 +1,33 @@
-$(".lblRdo").click(function(e){
+$("a.genderSelection").click(function(e){
+	e.preventDefault();
+	$("a.genderSelection.active").removeClass("active");
+	$(this).addClass("active");
 	
-	updateStackedBarChart($(e.currentTarget).find("input[name=rdoGender]").attr("value"),"2012");
+	updateStackedBarChart();
 });
 
 $("a.yearSelection").click(function(e){
 	e.preventDefault();
-	/* $(this).toggleClass("active"); */
 	$("a.yearSelection.active").removeClass("active");
 	$(this).addClass("active");
 	
-	updateStackedBarChart($("input[name=rdoGender]").attr("value"),$(this).attr("data-value"));
+	updateStackedBarChart();
 });
+
+
+
+function getArguments(){
+	var data = {};
+	data["gender"] = $("a.genderSelection.active").attr("data-value");
+	data["year"] = $("a.yearSelection.active").attr("data-value") || "2012";
+	data["cause"] = $("#tblLegend tr td.active").html() || null;
+
+	var filterInfo = { "G_T" : "Male and Female", "M_T" : "Only Males" , "F_T":"Only Females"};
+	var innerText = '<span class="label label-info">' + "Population :" + filterInfo[data["gender"]] + '</span>';
+	innerText = innerText +'<span class="label label-info">' + " Year : " + data["year"] + '</span>';
+	if(data["cause"])
+		innerText = innerText +'<span class="label label-info">' + " Cause : " + data["cause"] + '</span>';
+	$("#spnInfo").html(innerText);
+
+	return data;
+}
